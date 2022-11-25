5af6b0ae6c433679d48ab6c5dfcb0e6c4abc9a92
Durvaldo Gonçalves Marques <durvaldomarques@gmail.com>
2022-11-25 08:54:16 -0300

    Aula 4 vídeo 2

--- pages/faq.js
+++ pages/faq.js
@@ -1,8 +1,16 @@
-import { useEffect, useState } from 'react';
 import Link from '../src/components/Link';
 
-export async function getStaticProps() {
+// SSG - Static Site Generation 
+// SSR - Server Side Rendering
+// ISG - Incremental Static Generation
 
+// export async function getServerSideProps() {
+//     console.log("Roda cada asceeo que você recebe")
+//     só faz server side se precisar muito
+// }
+
+export async function getStaticProps() {
+    // console.log("Roda somente em bild time")
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
     const faq = await fetch(FAQ_API_URL)
         .then((resposta) => {
